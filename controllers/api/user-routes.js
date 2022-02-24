4303e75648bde20051c0ebd7053e369a8452aaa6
taylor.holmes <taylorh2343@gmail.com>
2022-02-23 23:28:38 -0600

    added get all and bcrypt package

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const { User, Post, Comment, Vote } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(userData => res.json(userData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
