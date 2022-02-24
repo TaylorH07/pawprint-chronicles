921aa4dc572a16d7bfcb13ea1e7e437886acca61
taylor.holmes <taylorh2343@gmail.com>
2022-02-24 00:51:59 -0600

    begen comments CRUD route w/ descriptions

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
 
+// GET all comments
+router.get('/', (req, res) => {
+
+})
+
+// POST - create comment
+router.post('/', withAuth, (req, res) => {
+
+})
+
+//DELETE  - delete comment
+router.delete('/:id', withAuth, (req, res) => {
+    
+})
+
+
 module.exports = router;
