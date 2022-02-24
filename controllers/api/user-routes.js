5c8e48f4e5507c39a9c148053fc88cef06422c36
taylor.holmes <taylorh2343@gmail.com>
2022-02-24 14:50:34 -0600

    completed user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -57,27 +57,107 @@ router.get('/:id', (req, res) => {
 
 // POST - create user route
 router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(userData => {
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.loggedIn = true;
 
-})
+            res.json(userData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // POST - login route
 router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(userData => {
+        if (!userData) {
+            res.status(400).json({ message: 'No pup parents with that email address' });
+            return;
+        }
 
-})
+        const validPassword = userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password, Try again :)' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: userData, message: 'Congragulations, you are now logged in!' })
+        })
+    })
+});
 
 // POST - logout route
 router.post('/logout', (req, res) => {
-
-})
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 // PUT - update by User id 
 router.put('/:id', (req, res) => {
-
-})
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(userData => {
+        if (!userData) {
+            res.status(404).json({ message: 'No pup parent discovered with this id'})
+            return;
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // DELETE - delete by User id
 router.delete('/:id', (req, res) => {
-
-})
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(userData => {
+        if (!userData) {
+            res.status(404).json({ message: 'Sorry, no pup pawrents found with this id' });
+            return;
+        }
+        res.json(userData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
