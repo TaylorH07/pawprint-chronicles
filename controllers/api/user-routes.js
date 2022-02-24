76cdb1378b87dab04aed8de2a380a583082b155e
taylor.holmes <taylorh2343@gmail.com>
2022-02-23 23:51:44 -0600

    added get by user id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,6 +14,44 @@ router.get('/', (req, res) => {
 });
 
 // get by user id
-
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes:['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
+    })
+    .then(userData => {
+        if (!userData) {
+            res.status(404).json({ message: 'No pup parents with this ID'})
+            return;
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
