4afbc10f020c3cfab6662ba4e0546fdf4fcac53d
Hiba Ali <hiba.mumin22@gmail.com>
2022-02-24 20:11:43 -0600

    created more associations, connected user to vote directly

--- models/index.js
+++ models/index.js
@@ -19,6 +19,25 @@ User.belongsToMany(Post, {
     onDelete: 'SET NULL'
   });
 
+  Vote.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Vote.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+  });
+
+ 
   
 
 
