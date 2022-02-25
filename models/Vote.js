c950c96aafc92903396e2ee14c5e1d3efc1cfccc
Hiba Ali <hiba.mumin22@gmail.com>
2022-02-24 19:44:25 -0600

    id model

--- models/Vote.js
+++ models/Vote.js
@@ -2,3 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Vote extends Model {}
+
+Vote.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
