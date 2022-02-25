0a4f7fe544f4777ce53407e70df244ec4ae63951
taylor.holmes <taylorh2343@gmail.com>
2022-02-24 20:59:39 -0600

    added controllers index

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 
+const homeRoute = require('./home-routes');
 const dashboardRoute = require('./dashboard-routes');
-const homeRoute = require('./home-routes')
+const apiRoute = require("./api")
+
+router.use('/', homeRoute);
+router.use('/dashboard', dashboardRoute);
+router.use('/api', apiRoute);
+
 
 module.exports = router;
