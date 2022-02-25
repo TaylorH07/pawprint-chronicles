8026eb4474f7d6392bb3c504537de7771080a11b
taylor.holmes <taylorh2343@gmail.com>
2022-02-24 20:08:46 -0600

    added comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,19 +1,18 @@
 const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-// GET all comments
-router.get('/', (req, res) => {
-
-})
 
 // POST - create comment
 router.post('/', withAuth, (req, res) => {
-
-})
-
-//DELETE  - delete comment
-router.delete('/:id', withAuth, (req, res) => {
-    
-})
+    Comment.create({ ...req.body, userId: req.session.userId })
+    .then(newComment => {
+        res.json(newComment);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 
 
 module.exports = router;
