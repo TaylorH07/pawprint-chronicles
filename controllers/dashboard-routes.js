d30887b0dde94ade205fe475b52bd2995e000ff1
taylor.holmes <taylorh2343@gmail.com>
2022-02-24 20:56:32 -0600

    added dash and home routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,3 +1,46 @@
 const router = require('express').Router();
+const { Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            userId: req.session.userId
+        }
+    })
+    .then(postData => {
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('all-posts-admin', {
+            layout: 'dashboard',
+            posts
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.redirect('login');
+    });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id)
+    .then(postData => {
+        if (postData) {
+            const post = postData.get({ plain: true });
+
+            res.render('edit-post', {
+                layout: 'dashboard',
+                post
+            });
+        }
+        else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
 const router = require('express').Router();
 
+const dashboardRoute = require('./dashboard-routes');
+const homeRoute = require('./home-routes')
+
 module.exports = router;
