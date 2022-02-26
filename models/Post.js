45a35c562fda6f8ee604ffdcce8b36fd387435f1
Hiba Ali <hiba.mumin22@gmail.com>
2022-02-25 20:33:52 -0600

    added remaining fields/columnand export expression

--- models/Post.js
+++ models/Post.js
@@ -13,3 +13,33 @@ Post.init(
         primaryKey: true,
         autoIncrement: true
       },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      post_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isURL: true
+        }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+
+  
+//   export expressions
+  module.exports = Post;
