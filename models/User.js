c5986fee29e6921e45ac9f47397d518cef1d5f68
Hiba Ali <hiba.mumin22@gmail.com>
2022-02-25 22:25:16 -0600

    created fields/columns for user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+// import bcrypt
+const bcrypt = require('bcrypt');
+
+
+// create fields/columns for User model
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      }
+    },
+    {
+      hooks: {
+  
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
