9d54bbf8466d2d1083d0aeb5cb4409023101b045
Hiba Ali <hiba.mumin22@gmail.com>
2022-02-25 20:23:45 -0600

    created field/column to initialize the post model

--- models/Post.js
+++ models/Post.js
@@ -3,3 +3,13 @@ const sequelize = require('../config/connection');
 
 // create our Post model
 class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
