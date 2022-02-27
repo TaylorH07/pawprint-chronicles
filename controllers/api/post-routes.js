f4663578ed5b702f5e1ab2b2a6a76463c5b3a2b2
taylor.holmes <taylorh2343@gmail.com>
2022-02-27 14:12:26 -0600

    updated post and comment routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,16 +1,52 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+// GET - get all user posts
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => res.json(postData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+  
 
 // POST - create post
 router.post('/', withAuth, (req, res) => {
-    const body = req.body;
-    console.log(req.session.userId);
-    Post.create({ ...body, userId: req.session.userId })
-    .then(newPost => {
-        res.json(newPost);
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.session.user_id
     })
+    .then(postData => res.json(postData))
     .catch(err => {
+        console.log(err);
         res.status(500).json(err);
     });
 });
@@ -27,42 +63,46 @@ router.put('/upvote', withAuth, (req, res) => {
 
 // PUT - update post by id
 router.put('/:id', withAuth, (req, res) => {
-    console.log(req.body, req.params.id)
-    Post.update(req.body, {
-        where: {
-            id: req.params.id
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
         }
-    })
-    .then(affectedRows => {
-        if (affectedRows > 0) {
-            res.status(200).end();
+    )
+    .then(postData => {
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this pawrents id' });
+            return;
         }
-        else {
-            res.status(404).end();
-        }
+        res.json(postData);
     })
     .catch(err => {
+        console.log(err);
         res.status(500).json(err);
     });
 });
 
 //DELETE - delete post by id
 router.delete('/:id', withAuth, (req, res) => {
-    console.log(req.body, req.params.id)
+    console.log('id', req.params.id)
     Post.destroy({
         where: {
             id: req.params.id
         }
     })
-    .then(affectedRows => {
-        if (affectedRows > 0) {
-            res.status(200).end();
+    .then(postData => {
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this pawrents id' });
+            return;
         }
-        else {
-            res.status(404).end();
-        }
+        res.json(postData);
     })
     .catch(err => {
+        console.log(err);
         res.status(500).json(err);
     });
 });
