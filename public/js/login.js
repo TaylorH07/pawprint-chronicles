278a2409e17996ecaea91dbaa078e7ffe3cc81d9
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-02-26 20:31:44 -0600

    cleaned up login

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,23 @@
-function loginFormHandler(event) {
+ async function loginFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-input-login').value.trim();
     const password = document.querySelector('#password-input-login').value.trim();
+
+    if (username && password) {
+       const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
 }
 document.querySelector('.login-form').addEventListener('submit',loginFormHandler);
