08c5fca8c5ec9a08d7f5fba53909c7c5bb9e67f7
mflanigan <mflanigantwualumn@gmail.com>
2022-03-02 17:30:49 -0600

    wrote missing handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,52 @@
- async function loginFormHandler(event) {
+async function loginFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-input-login').value.trim();
     const password = document.querySelector('#password-input-login').value.trim();
 
     if (username && password) {
-       const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
                 username,
                 password
             }),
-            headers: { 'Content-Type': 'application/json'}
+            headers: { 'Content-Type': 'application/json' }
         });
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard/');
         } else {
             alert(response.statusText);
         }
     }
 }
-document.querySelector('.login-btn').addEventListener('submit',loginFormHandler);
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-btn').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
