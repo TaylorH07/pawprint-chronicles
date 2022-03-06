15136e0f4bd825eae45fe1d3338dd61e8b95df73
Hiba Ali <hiba.mumin22@gmail.com>
2022-03-06 11:04:29 -0600

    tried to fix the errors );

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post-body"]').value;
+    const post_url = document.querySelector('input[name="post-url"]').value;
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
@@ -16,7 +16,7 @@ async function newFormHandler(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard/new');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
