a88bc606d59c25e937e16bbee610bf5b3e0506a1
mflanigan <mflanigantwualumn@gmail.com>
2022-03-06 10:31:12 -0600

    fixed delete btn id

--- public/js/delete.js
+++ public/js/delete.js
@@ -15,4 +15,4 @@ async function deleteFormHandler(event) {
     }
   }
   
-  document.querySelector('.delete-btn').addEventListener('click', deleteFormHandler);
+  document.querySelector('#delete-btn').addEventListener('click', deleteFormHandler);
