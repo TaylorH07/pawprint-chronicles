61b4e8da3e6468317f125c0945cb4093d1421807
mflanigan <mflanigantwualumn@gmail.com>
2022-03-06 10:23:56 -0600

    fixed replace to dashboard

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -16,7 +16,7 @@ async function newFormHandler(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard/new');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
